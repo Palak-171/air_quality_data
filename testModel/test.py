4021043ea388a6eb4e9faee8b4f57d1f33f08772
Palak Bhangotra <72605947+Palak-171@users.noreply.github.com>
2021-01-27 01:42:17 +0530

    Add files via upload

---
+++ testModel/test.py
@@ -0,0 +1,11 @@
+#using the model saved from hard disk 
+import pickle 
+# load the model from disk
+filename = 'DTModel.pkl'
+loaded_model = pickle.load(open(filename, 'rb'))
+P =[[9,31,51,205.25]]
+print(loaded_model.predict(P))
+P2 =[[2,5.8,17,36]]
+print(loaded_model.predict(P2))
+P3 = [[90.3,85.2,34.7,45.8]]
+print(loaded_model.predict(P3))
